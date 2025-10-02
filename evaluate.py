e4151c722c3967887604aa403a7dc7fd4436865c
Shervin Dadashzadeh <shervin.dadashzade7988@gmail.com>
2025-10-02 09:11:54 +0330

    Changing evaluate.py to be comparable with AdaPPI

--- evaluate.py
+++ evaluate.py
@@ -45,41 +45,117 @@ class Evaluation:
         
         self.filtered_complexes = filtered_complexes
     
-    def evalute(self, algorithm_complexes, threshold_na=0.25):
-        NCP = 0
-        NCB = 0
+    def _precision_score(self, predicted_complex, reference_complex, predicted_num):
+        match_info_list = []
+        unmatch_pred_list = []
+        number=0
+        for i, pred in enumerate(predicted_complex):
+            overlapscore=0.0
+            tmp_max_score_info = None
+            for j, ref in enumerate(reference_complex):
+                set1 = set(ref)
+                set2 = set(pred)
+                overlap = set1 & set2
+                score = float((pow(len(overlap), 2))) / float((len(set1) * len(set2)))
+                if score > overlapscore:
+                    overlapscore = score
+                    # find max score
+                    tmp_max_score_info = {
+                        'pred': pred, 'true': ref, 
+                        'overlap_score': overlapscore,
+                        'pred_id': i, 'true_id': j, 
+                    }
+            if overlapscore > 0.25:
+                number = number + 1
+                if tmp_max_score_info is not None:
+                    match_info_list.append(tmp_max_score_info)                
+            else:
+                unmatch_pred_list.append(pred)
+
+        return number/(predicted_num+1e-4), number, match_info_list, unmatch_pred_list
     
-        algorithm_to_reference = {}
-        for a_idx, a_comp in enumerate(algorithm_complexes):
-            for idx, comp in enumerate(self.filtered_complexes):
-                if len(a_comp) == 0:
-                    continue
-                a_comp = set(a_comp)
-                comp = set(comp)
-                NA = len(a_comp.intersection(comp))**2 / (len(a_comp) * len(comp))
-                if NA >= threshold_na:
-                    NCP+=1
-                    algorithm_to_reference[a_idx] = idx
-                    break
+    def _recall_score(self, predicted_complex, reference_complex, reference_num):
+        match_info_list = []
+        unmatch_pred_list = []
+        c_number = 0
+        for i, ref in enumerate(reference_complex):
+            overlapscore=0.0
+            tmp_max_score_info = None
+            for j, pred in enumerate(predicted_complex):
+                set1 = set(ref)
+                set2 = set(pred)
+                overlap = set1 & set2
+                score = float((pow(len(overlap), 2))) / float((len(set1) * len(set2)))
+            
+                if score > overlapscore:
+                    overlapscore = score
+
+                    tmp_max_score_info = {
+                        'pred': pred, 'true': ref, 
+                        'overlap_score': overlapscore,
+                        'pred_id': j, 'true_id': i, 
+                    }
+            
+            if overlapscore > 0.25:
+                c_number = c_number+1
+                if tmp_max_score_info is not None:
+                    match_info_list.append(tmp_max_score_info)
+            else:
+                unmatch_pred_list.append(pred)
+
+        return c_number/(reference_num+1e-4), c_number, match_info_list, unmatch_pred_list
+
+
+    def _acc_score(self, predicted_complex, reference_complex):
+        # sn
+        T_sum1=0.0
+        N_sum=0.0   # the number of proteins in reference complex
+        for i in reference_complex:
+            max=0.0
+            for j in predicted_complex:
+                set1=set(i)
+                set2=set(j)
+                overlap=set1&set2
+                if len(overlap)>max:
+                    max=len(overlap)
+            T_sum1=T_sum1+max
+            N_sum=N_sum+len(set1)
+
+        # ppv
+        T_sum2=0.0
+        T_sum=0.0
+        for i in predicted_complex:
+            max=0.0
+            for j in reference_complex:
+                set1=set(i)
+                set2=set(j)
+                overlap=set1&set2
+                T_sum=T_sum+len(overlap)
+                if len(overlap)>max:
+                    max=len(overlap)
+            T_sum2=T_sum2+max
     
-        reference_to_algorithm = {}
-        for idx, comp in enumerate(self.filtered_complexes):
-            for a_idx, a_comp in enumerate(algorithm_complexes):
-                if len(a_comp) == 0:
-                    continue
-                a_comp = set(a_comp)
-                comp = set(comp)
-                NA = len(a_comp.intersection(comp))**2 / (len(a_comp) * len(comp)) 
-                if NA >= threshold_na:
-                    NCB+=1
-                    reference_to_algorithm[idx] = a_idx
-                    break
-        
-        recall = NCB/len(self.filtered_complexes)
-        precision = NCP/len(algorithm_complexes)
+        Sn = float(T_sum1) / float(N_sum)
+        PPV = float(T_sum2) / float(T_sum+1e-5)
+        Acc = pow(float(Sn*PPV), 0.5)
+        return Acc
+
+    def evalute(self, algorithm_complexes):
+        acc = self._acc_score(algorithm_complexes, self.filtered_complexes)
+        reference_num = len(self.filtered_complexes)
+        predicted_num = len(algorithm_complexes)
+
+        precision, p_num, _, _ = self._precision_score(algorithm_complexes, self.filtered_complexes, predicted_num)
+        recall, r_num, _, _ = self._recall_score(algorithm_complexes, self.filtered_complexes, reference_num)
+
+        f1 = float((2*precision*recall)/(precision+recall+1e-5))
+
+
         return {
-            'NCP':NCP,
-            'NCB':NCB,
-            'Recal':recall,
-            'Precision':precision
+            'Precision': precision,
+            'Recall': recall,
+            'Acc': acc,
+            'F1': f1,
+            'NCP':p_num,
+            'NCB': r_num,
         }
