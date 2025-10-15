67a75a8a312ac3f17f6e22ceae2cb3ba0562fba0
Samaneh Tejerloo <tejerloo.samaneh@gmail.com>
2025-10-15 11:15:50 +0330

    TADW Biogrid and DIP supplementary files added.

---
+++ tests/lstm_test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 15 10:30:19 2025
+
+@author: Samaneh
+"""
+
+#%%
+import torch
+import torch.nn as nn
+#%%
+n = 200
+k = 3
+d = 256
+x = torch.rand(k,n,d)
+#%%
+lstm = nn.LSTM(input_size=d, hidden_size=d, num_layers=1, batch_first=False, bidirectional=True)
+#%%
+out, h_n = lstm(x)
+#%%
