d1d265953ea9afa268b7e6b531922802e5d5f465
Samaneh Tejerloo <tejerloo.samaneh@gmail.com>
2025-10-26 12:30:34 +0330

    JKNETConcat implemented

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
 from models.simple_gcn import SimpleGCN, SimpleGAT
-from models.jknet import JKNetGATWith3Layers, LSTMAttentionJKNETGAT, AttentionJKNETGAT
+from models.jknet import JKNetGATWith3Layers, LSTMAttentionJKNETGAT, AttentionJKNETGAT, JKNetGATConcat
 
--- models/jknet.py
+++ models/jknet.py
@@ -30,6 +30,27 @@ class JKNetGATWith3Layers(torch.nn.Module):
         out_pooled, _  = torch.max(out, dim=0)
         return out_pooled
 
+class JKNetGATConcat(torch.nn.Module):
+    def __init__(self, embedding_dim=128, intermediate_dim=256, encoding_dim=128, heads=1, dropout=0):
+        super().__init__()
+        self.heads = heads
+        self.embedding_dim = embedding_dim
+        self.intermediate_dim = intermediate_dim
+
+        self.gatconv1 = GATConv(embedding_dim, intermediate_dim, heads=heads, concat=False, dropout=dropout)
+        self.gatconv2 = GATConv(intermediate_dim, intermediate_dim, heads=heads, concat=False, dropout=dropout)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x1 = self.gatconv1(x, edge_index)
+        x1 = F.relu(x1)
+        
+        x2 = self.gatconv2(x1, edge_index)
+        x2 = F.relu(x2)
+        
+        
+        out = torch.cat([x1,x2], dim=-1)
+        return out
 
 class LSTMAttentionJKNETGAT(torch.nn.Module):
     def __init__(self, embedding_dim=128, intermediate_dim=256, encoding_dim=128, heads=1, dropout=0):
@@ -108,7 +129,7 @@ if __name__ == '__main__':
     edge_index = torch.LongTensor(data.edges_index).T
     from torch_geometric.data import Data
     data = Data(x=features, edge_index=edge_index)
-    net = AttentionJKNETGAT(embedding_dim=data.num_features)
+    net = LSTMAttentionJKNETGAT(embedding_dim=data.num_features, intermediate_dim=512, encoding_dim=512, heads=4)
     out = net(data)
     print(out.shape)
 # %%
